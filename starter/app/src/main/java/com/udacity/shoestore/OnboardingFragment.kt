ead91b0b58f99d4ba159a78d779261310939cb92
trunghieuluongit <trunghieuluongit@gmail.com>
2022-05-09 21:25:06 +0700

    Set DataBindingUtil in build.gradle && Use DataBindingUtil to inflate every layout

---
+++ starter/app/src/main/java/com/udacity/shoestore/OnboardingFragment.kt
@@ -0,0 +1,22 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentOnboardingBinding
+
+
+class OnboardingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentOnboardingBinding>(layoutInflater,R.layout.fragment_onboarding,container,false)
+        return binding.root
+    }
+
+}
